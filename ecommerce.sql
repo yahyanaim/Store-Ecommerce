d1640a7b45167220d04227001f01c6f26843281e
yahyanaim <57252787+yahyanaim@users.noreply.github.com>
2021-07-01 15:29:42 +0100

    E-com

--- ecommerce.sql
+++ ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : sam. 27 juin 2020 à 20:21
+-- Généré le : sam. 27 juin 2021 à 20:21
 -- Version du serveur :  10.4.11-MariaDB
 -- Version de PHP : 7.4.5
 
